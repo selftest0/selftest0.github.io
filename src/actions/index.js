6474746c6280e45e6fd37275a35fc8619efd9a2f
Борис Южаков <boris.yuzhakov@altarix.ru>
2018-12-07 14:21:12 +0300

    Updates

--- src/actions/index.js
+++
@@ -1,2 +0,0 @@
-// const access_token = localStorage.getItem('access_token');
-// export const stringify = (params = {}) => `?${Object.entries({ ...params, access_token }).map(([ key, value ]) => (value === null ? key : `${key}=${value}`)).join('&')}`;
