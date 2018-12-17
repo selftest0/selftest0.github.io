938129b40ed36da29257000357ad89519bc9d365
Борис Южаков <boris.yuzhakov@altarix.ru>
2018-12-17 18:01:36 +0300

    Updates

---
+++ precache-manifest.0ab5f67773bad7b7a25ad7bb6c8cd790.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1abac85870b8967f64a3df86c6bb541e",
+    "url": "/static/media/survey.1abac858.svg"
+  },
+  {
+    "revision": "b31fdba0fb1b9f2f2700eb87b0d7ef19",
+    "url": "/static/media/ok.b31fdba0.svg"
+  },
+  {
+    "revision": "6b2f87a4312e11bb7af5f19c5948bedf",
+    "url": "/static/media/loading.6b2f87a4.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "89c9ea3485a9c9228208",
+    "url": "/static/js/main.89c9ea34.chunk.js"
+  },
+  {
+    "revision": "f7df6c2d6915440d564f",
+    "url": "/static/js/1.f7df6c2d.chunk.js"
+  },
+  {
+    "revision": "89c9ea3485a9c9228208",
+    "url": "/static/css/main.ff122e8c.chunk.css"
+  },
+  {
+    "revision": "e3537a581061486004dd925964952a20",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.71b9fd140ed62d91a69d2cdb7364a7dd.js"
+  "/precache-manifest.0ab5f67773bad7b7a25ad7bb6c8cd790.js"
 );
 
 workbox.clientsClaim();
